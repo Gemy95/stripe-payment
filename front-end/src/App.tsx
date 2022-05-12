8deb9e1b26afddefcff7112aae3c3bb4f5578045
Ali Gamal <ali.gamal@beyond-solution.com>
2022-05-12 22:42:56 +0200

    NF: add style for payment form component

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -4,11 +4,21 @@ import PaymentForm from './PaymentForm';
  
 const stripePromise = loadStripe(process.env.REACT_APP_STRIPE_PUBLISHABLE_KEY as string);
  
+const ELEMENTS_OPTIONS = {
+  fonts: [
+    {
+      cssSrc: "https://fonts.googleapis.com/css?family=Roboto"
+    }
+  ]
+};
+
 function App() {
   return (
-    <Elements stripe={stripePromise}>
+    <div className="AppWrapper">
+    <Elements stripe={stripePromise} options={ELEMENTS_OPTIONS}>
       <PaymentForm />
     </Elements>
+  </div>
   );
 }
  
--- front-end/src/PaymentForm/index.tsx
+++ front-end/src/PaymentForm/index.tsx
@@ -3,17 +3,38 @@ import { CardElement, useStripe, useElements } from "@stripe/react-stripe-js";
 import axios from "axios";
 
 
-const PaymentForm = () => {
+const CARD_OPTIONS = {
+  style: {
+    base: {
+      iconColor: "#c4f0ff",
+      color: "#fff",
+      fontWeight: 500,
+      fontFamily: "Roboto, Open Sans, Segoe UI, sans-serif",
+      fontSize: "16px",
+      fontSmoothing: "antialiased",
+      ":-webkit-autofill": {
+        color: "#fce883"
+      },
+      "::placeholder": {
+        color: "#87bbfd"
+      }
+    },
+    invalid: {
+      iconColor: "#ffc7ee",
+      color: "#ffc7ee"
+    }
+  }
+};
 
-  const [amount, setAmount] = React.useState<number>(1);
+
+const PaymentForm = () => {
+  const [amount, setAmount] = React.useState<number>(100);
   const stripe = useStripe();
   const elements = useElements();
-
 
   const handleInputAmountChange = (e: any) => {
     setAmount(parseInt(e.target.value));
   };
-
 
   const HandleSubmit = async (event: FormEvent) => {
     event.preventDefault();
@@ -62,22 +83,26 @@ const PaymentForm = () => {
       });
   };
   return (
-    <form
+    <form className="Form"
       onSubmit={(event) => {
         HandleSubmit(event);
       }}
     >
-      <div>
-        <label>Enter Amount that you want to pay:</label>
-        <input
+      <div className="FormRow">
+        <label className="FormRowLabel">Enter Amount To Pay :</label>
+      </div>
+      <div className="FormRow">
+      <input className="FormRowInput"
           type="number"
           min={100}
           value={amount}
           onChange={handleInputAmountChange}
         />
+        </div>
+      <div className="FormRow">
+        <CardElement options={CARD_OPTIONS} />
       </div>
-      <CardElement />
-      <button type="submit">Pay</button>
+      <button className={`SubmitButton`} type="submit" > Pay </button>
     </form>
   );
 };
